5859415f67e1ed1366c1f41e0f3bccbbe976a400
Edgar <tanotuff12@gmail.com>
2017-03-02 16:12:49 -0600

    fix(grunt) carga automatica de tareas

--- built-helpers/config.js
+++ built-helpers/config.js
@@ -1,16 +1,22 @@
 
 
 module.exports = ( _grunt, folder ) => {
+    const grunt_init_config = {}
 
     const path = require('path')
     const fs = require('fs')
 
     const PATH = path.join( __dirname, folder )
     const FILES = fs.readdirSync(PATH)
-    
+
     FILES.forEach((file) => {
         if( `${file}`.search(".js") > -1 ){
-            require( `${PATH}/${file}`)(_grunt)
+            var task = require( `${PATH}/${file}`)( _grunt )
+            if( task ){
+                grunt_init_config[task.name] = task.config
+            }
         }
-    });
+    })
+
+     _grunt.initConfig( grunt_init_config )
 }
